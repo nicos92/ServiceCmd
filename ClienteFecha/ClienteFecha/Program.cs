4936f13019265ce2f9fc3b8feface766ec3e82d3
nicos92 <nicolassandoval140692@gmail.com>
2025-08-06 08:02:49 -0300

    cambios

--- ClienteFecha/ClienteFecha/Program.cs
+++ ClienteFecha/ClienteFecha/Program.cs
@@ -14,81 +14,88 @@ class Cliente
         const string CLAVE = "algunaclave";
         const int INTENTOS = 3;
         int intento = 0;
-
-        while (intento <= INTENTOS)
+        char opcion = '1';
+        while (opcion != '0')
         {
-            System.Console.WriteLine("Ingrese la clave:");
-            string claveIngresada = LeerClaveConAsteriscos();
-            if (claveIngresada != CLAVE)
+            System.Console.WriteLine("1- enviar comando\n0-Salir");
+            opcion = Console.ReadKey(true).KeyChar;
+            if (opcion == '1')
             {
-                intento++;
-                if (intento == INTENTOS)
+                while (intento <= INTENTOS)
                 {
-                    System.Console.WriteLine("Demasiados intentos fallidos");
-                    return;
+                    System.Console.WriteLine("Ingrese la clave:");
+                    string claveIngresada = LeerClaveConAsteriscos();
+                    if (claveIngresada != CLAVE)
+                    {
+                        intento++;
+                        if (intento == INTENTOS)
+                        {
+                            System.Console.WriteLine("Demasiados intentos fallidos");
+                            return;
+                        }
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        System.Console.WriteLine("Clave incorrecta\nIntente nuevamente\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    }
+                    else break;
                 }
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                System.Console.WriteLine("Clave incorrecta\nIntente nuevamente\n");
-                Console.ForegroundColor = ConsoleColor.White;
-
-            }
-            else break;
-        }
-        string comando;
-        intento = 0;
-        bool comandoOk = false;
-        do
-        {
-
-            Console.Write("Comando a enviar: ");
-            comando = Console.ReadLine() ?? " ";
-            if (!Regex.IsMatch(comando, @"^(date|time)\s(\d{2}/\d{2/\d{4}|\d{2}:\d{2})"))
-            {
-
-                intento++;
-                if (intento == INTENTOS)
+                string comando;
+                intento = 0;
+                bool comandoOk = false;
+                do
                 {
-                    System.Console.WriteLine("Demasiados intentos fallidos");
-                    return;
+
+                    Console.Write("Comando a enviar: ");
+                    comando = Console.ReadLine() ?? " ";
+                    if (!Regex.IsMatch(comando, @"^(date|time)\s(\d{2}/\d{2/\d{4}|\d{2}:\d{2})"))
+                    {
+
+                        intento++;
+                        if (intento == INTENTOS)
+                        {
+                            System.Console.WriteLine("Demasiados intentos fallidos");
+                            return;
+                        }
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                        System.Console.WriteLine("Comando incorrecto\nIntente nuevamente\n");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    }
+                    else
+                    {
+                        comandoOk = true;
+                    }
+
+
+
+                } while (!comandoOk);
+
+
+                string mensaje = $"{CLAVE}|{comando}";
+                foreach (var item in IpsServidores)
+                {
+                    try
+                    {
+                        using TcpClient cliente = new TcpClient(item, puerto);
+                        using NetworkStream stream = cliente.GetStream();
+                        using StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+                        using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+                        writer.WriteLine(mensaje);
+
+                        Console.WriteLine($"Respuesta del servidor {item}:");
+                        string respuesta = reader.ReadToEnd(); // Lee todo lo que responde el servidor
+                        Console.WriteLine(respuesta);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error al conectar al servidor {item}: " + ex.Message);
+                    }
                 }
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-
-                System.Console.WriteLine("Comando incorrecto\nIntente nuevamente\n");
-                Console.ForegroundColor = ConsoleColor.White;
-
-            }
-            else
-            {
-                comandoOk = true;
-            }
-
-
-
-        } while (!comandoOk);
-
-
-        string mensaje = $"{CLAVE}|{comando}";
-        foreach (var item in IpsServidores)
-        {
-            try
-            {
-                using TcpClient cliente = new TcpClient(item, puerto);
-                using NetworkStream stream = cliente.GetStream();
-                using StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
-                using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-
-                writer.WriteLine(mensaje);
-
-                Console.WriteLine($"Respuesta del servidor {item}:");
-                string respuesta = reader.ReadToEnd(); // Lee todo lo que responde el servidor
-                Console.WriteLine(respuesta);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error al conectar al servidor {item}: " + ex.Message);
             }
         }
-
     }
 
     public static string LeerClaveConAsteriscos()
